738b967b1d71c9e3eea226c357046026045237b7
Xie Yijie <16251296@life.hkbu.edu.hk>
2021-01-02 17:33:35 +0800

    Store Mapbox API key to the configuration file

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function(environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoic3RldmVuZGIiLCJhIjoiY2tqZmkzNHkxMTJiNDJ4bnZ3ajZ5Yjd3MCJ9.iW4FKToXmTJEJWqLeR06Ug';
+
   return ENV;
 };
