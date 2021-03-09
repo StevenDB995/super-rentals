5503402292e03df644df20f19e95b842d367edb0
Xie Yijie <16251296@life.hkbu.edu.hk>
2021-03-09 12:44:44 +0800

    User Ember Data to manage data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,27 +1,12 @@
 import Route from '@ember/routing/route';
+import {
+  inject as service
+} from '@ember/service';
 
-const COMMUNITY_CATEGORIES = [
-    'Condo',
-    'Townhouse',
-    'Apartment'
-  ];
-  
-  export default class IndexRoute extends Route {
-    async model() {
-      let response = await fetch('/api/rentals.json');
-      let { data } = await response.json();
-  
-      return data.map(model => {
-        let { id, attributes } = model;
-        let type;
-  
-        if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-          type = 'Community';
-        } else {
-          type = 'Standalone';
-        }
-  
-        return { id, type, ...attributes };
-      });
-    }
+export default class IndexRoute extends Route {
+  @service store;
+
+  async model() {
+    return this.store.findAll('rental');
   }
+}
